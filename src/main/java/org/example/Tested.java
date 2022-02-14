2fd21342c078d39b6193bc98ad76c6a0b7a906a1
xavier <xys1954331789@163.com>
2022-02-14 15:15:30 +0800

    空

---
+++ src/main/java/org/example/Tested.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @Description TODO
+ * @Author xavier
+ * @Date 2022/2/14 15:14
+ */
+public class
+Tested {
+    public static void main(String[] args) {
+        System.out.println("002");
+    }
+}
