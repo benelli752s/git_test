34e67236915e0f5de161742c59b267faa330ce5c
xavier <xys1954331789@163.com>
2022-02-14 14:16:24 +0800

    第二次提交

---
+++ src/main/java/org/example/Tets01.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @Description TODO
+ * @Author xavier
+ * @Date 2022/2/14 13:34
+ */
+public class Tets01 {
+    public static void main(String[] args) {
+        System.out.println("test01第一次提交");
+    }
+}
